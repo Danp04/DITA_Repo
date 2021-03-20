297060f8dcacadc31a313f60a5bead345856ea0d
Dana <dpiec1@stu.vistula.edu.pl>
2021-03-20 17:04:09 +0100

    deleting files

--- oxygen-webhelp/app/search/index/index-3.js
+++
@@ -1,4 +0,0 @@
-define(function () {
-// Auto generated index for searching.
-return {"starting":"10*30*0$3","system":"2*30*0$4,3*30*0$4","team":"5*30*1$3","teams":"0*31*3$7$c,1*31*2$5$b,2*30*2$6,3*30*2$6,6*30*2$5","use":"0*0*a,1*30*0$3","video":"7*30*1$4","windows":"3*30*3$7"};
-});
