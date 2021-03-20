8dc3f174904cda48209950a0dddd063d76fb34ee
Dana <dpiec1@stu.vistula.edu.pl>
2021-03-20 18:17:11 +0100

    changing files

--- oxygen-webhelp/app/search/index/index-1.js
+++ oxygen-webhelp/app/search/index/index-1.js
@@ -1,4 +1,4 @@
 define(function () {
 // Auto generated index for searching.
-return {"breakout":"4*30*1$4","calls":"7*30*2$5","chat":"10*30*2$5","creating":"4*30*0$3,5*30*0$2","files":"8*30*1$3","getting":"0*30*0$4,1*0*8","information":"0*5*9,1*5*7","installing":"6*30*0$3","mac":"2*30*3$7"};
+return {"breakout":"4*30*1$4","calls":"7*30*2$5","chat":"10*30*2$5","creating":"4*30*0$3,5*30*0$2","files":"8*30*1$3","getting":"0*30*0$4,1*0*8,11*30*0$4","guide":"0*10*-1$c,11*10*-1$c","information":"0*5*f,1*5*7","installing":"6*30*0$3","mac":"2*30*3$7"};
 });
--- oxygen-webhelp/app/search/index/index-2.js
+++ oxygen-webhelp/app/search/index/index-2.js
@@ -1,4 +1,4 @@
 define(function () {
 // Auto generated index for searching.
-return {"making":"7*30*0$3","microsoft":"0*31*2$6$b,1*31*1$4$a,6*30*1$4","new":"10*30*1$4","related":"0*5*8,1*5*6","requirements":"2*30*1$5,3*30*1$5","room":"4*30*2$5","sharing":"8*30*0$2","signing":"9*30*0$1","started":"0*30*1$5,1*0*9"};
+return {"making":"7*30*0$3","microsoft":"0*41*-1$2$6$9$h,1*31*1$4$a,6*30*1$4,11*40*-1$2$6$9","microsoft_teams_user_guide.pdf":"0*10*-1$8,11*10*-1$8","new":"10*30*1$4","pdf":"0*10*-1$d,11*10*-1$d","related":"0*5*e,1*5*6","requirements":"2*30*1$5,3*30*1$5","room":"4*30*2$5","sharing":"8*30*0$2","signing":"9*30*0$1"};
 });
